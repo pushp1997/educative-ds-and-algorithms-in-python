7ad23a84530183270df0299a5b10ec125a5f9bdf
Pushp Vashisht <pushptyagi1@gmail.com>
2022-08-14 01:25:17 +0530

    Added deletion methods & renamed module

--- Singly Linked List/linked_list.py
+++ Singly Linked List/linked_list.py
@@ -40,3 +40,23 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = prev_node.next
         prev_node.next = new_node
+
+    # Deletion
+    def delete_node(self, data: Any) -> None:
+        """Delete a node in the linked list"""
+        cur_node = self.head
+        prev_node: Node = None
+        while cur_node:
+            # If the 1st node is a match
+            if cur_node.data == data and prev_node is None:
+                self.head = cur_node.next
+                del cur_node
+                return
+            # If any other node is a match
+            elif cur_node.data == data and prev_node:
+                prev_node.next = cur_node.next
+                del cur_node
+                return
+            else:
+                prev_node = cur_node
+                cur_node = cur_node.next
